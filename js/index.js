fa0b503f514c342c5ff39b9b3df2f90f9ed964a3
Dmitriy <dmitriypruss21041984@gmail.com>
2021-06-01 20:16:45 +0300

    third commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-//  closure
+'use strict'
 
+// Closure
 // Напишите функцию addNum(n), которая вернёт другую функцию. Возвращенная 
 // функция должна складывать получаемый аргумент с аргументом n возвращающей функции.
 // Пояснения:
@@ -11,16 +12,14 @@ const result = add(10); // => 15
 console.log('result :>> ', result);
 
 const add2 = addNum(32);
-const result2 = add2(122); // => 154
+const result2 = add2(222); // => 254
 console.log('result2 :>> ', result2);
 
 const add3 = addNum();
-const result3 = add3(); // => 0
+const result3 = add3(1000); // => 1000
 console.log('result3 :>> ', result3);
 
 function addNum(n = 0) {
-    let num = 0;
-    console.log('num :>> ', num);
 
     return function(m = 0) {
         if (typeof n !== 'number' || typeof m !== 'number') {
@@ -29,9 +28,7 @@ function addNum(n = 0) {
         if (Number.isNaN(n) || Number.isNaN(m)) {
             throw new RangeError(`This value isn't correct. Enter another...`);
         };
-        
-        num = n;
-
-        return num + m;
+    
+        return n + m;
     };
 };
