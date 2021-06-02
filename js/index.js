c4691684c229be4f9c6a17b2e0a19870cd7bbae5
Dmitriy <dmitriypruss21041984@gmail.com>
2021-06-02 20:31:47 +0300

    fourth commit

--- js/index.js
+++ js/index.js
@@ -15,17 +15,24 @@ const add2 = addNum(32);
 const result2 = add2(222); // => 254
 console.log('result2 :>> ', result2);
 
-const add3 = addNum();
-const result3 = add3(1000); // => 1000
+const add3 = addNum('blablabla');
+const result3 = add3(1000); // => TypeError
 console.log('result3 :>> ', result3);
 
 function addNum(n = 0) {
 
+    if ( typeof n !== 'number' ) {
+        throw new TypeError(`This value is not a number`);
+    };
+    if ( Number.isNaN(n) ) {
+        throw new RangeError(`This value isn't correct. Enter another...`);
+    };
+
     return function(m = 0) {
-        if (typeof n !== 'number' || typeof m !== 'number') {
+        if ( typeof m !== 'number' ) {
             throw new TypeError(`This value is not a number`);
         };
-        if (Number.isNaN(n) || Number.isNaN(m)) {
+        if ( Number.isNaN(m) ) {
             throw new RangeError(`This value isn't correct. Enter another...`);
         };
     
